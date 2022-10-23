a4ff0d2dd8e9a41a275b492e6580ada031cce9b0
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-10-23 20:08:23 +0200

    Various deployment adjustments

--- apps/design/vite.config.ts
+++ apps/design/vite.config.ts
@@ -1,7 +1,5 @@
 import { whyframe } from '@whyframe/core';
 import { whyframeJsx } from '@whyframe/jsx';
-// @ts-expect-error No typings for adapter
-import vercel from 'solid-start-vercel';
 import solid from 'solid-start/vite';
 import UnoCss from 'unocss/vite';
 import { defineConfig } from 'vite';
@@ -11,7 +9,7 @@ export default defineConfig({
 		whyframe({ components: [{ name: 'Story', showSource: true }], defaultSrc: '/iframe' }),
 		whyframeJsx({ defaultFramework: 'solid' }),
 		UnoCss(),
-		solid({ adapter: vercel({ edge: true }) }),
+		solid(),
 	],
 	ssr: {
 		external: ['shiki'],
