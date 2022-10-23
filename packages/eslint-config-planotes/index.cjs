fe45a45356427c2a5004b7e686aaeae4078595b2
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-10-23 19:35:23 +0200

    Format files

--- packages/eslint-config-planotes/index.cjs
+++ packages/eslint-config-planotes/index.cjs
@@ -1,75 +1,73 @@
 module.exports = {
-  overrides: [
-    {
-      extends: [
-        "canonical",
-        "canonical/typescript",
-        "canonical/module",
-        "canonical/browser",
-        "canonical/prettier",
-      ],
-      files: "*.ts",
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-      rules: {
-        "@typescript-eslint/no-throw-literal": "off",
-        "@typescript-eslint/unbound-method": "off",
-        "canonical/filename-match-exported": "off",
-        "canonical/filename-match-regex": "off",
-        "import/no-unassigned-import": "off",
-        "no-restricted-imports": [
-          "error",
-          {
-            patterns: [
-              {
-                group: [".*"],
-                message: "Don't use relative imports",
-              },
-            ],
-          },
-        ],
-        "prettier/prettier": "error",
-        "unicorn/prevent-abbreviations": "off",
-      },
-    },
-    {
-      extends: [
-        "canonical",
-        "canonical/module",
-        "canonical/browser",
-        "canonical/jsx-a11y",
-        "canonical/typescript",
-        "plugin:solid/typescript",
-        "canonical/prettier",
-      ],
-      files: "*.tsx",
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-      rules: {
-        "@typescript-eslint/no-throw-literal": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/unbound-method": "off",
-        "jsx-a11y/label-has-associated-control": "off",
-        "canonical/filename-match-exported": "off",
-        "canonical/filename-match-regex": "off",
-        "import/no-unassigned-import": "off",
-        "no-restricted-imports": [
-          "error",
-          {
-            patterns: [
-              {
-                group: [".*"],
-                message: "Don't use relative imports",
-              },
-            ],
-          },
-        ],
-        "prettier/prettier": "error",
-        "unicorn/prevent-abbreviations": "off",
-      },
-    },
-  ],
-  plugins: ["solid", "import", "prettier"],
+	overrides: [
+		{
+			extends: ['canonical', 'canonical/typescript', 'canonical/module', 'canonical/browser', 'canonical/prettier'],
+			files: '*.ts',
+			parserOptions: {
+				project: './tsconfig.json',
+			},
+			rules: {
+				'@typescript-eslint/no-throw-literal': 'off',
+				'@typescript-eslint/unbound-method': 'off',
+				'@typescript-eslint/no-misused-promises': 'off',
+				'canonical/filename-match-exported': 'off',
+				'canonical/filename-match-regex': 'off',
+				'canonical/id-match': 'off',
+				'import/no-unassigned-import': 'off',
+				'no-restricted-imports': [
+					'error',
+					{
+						patterns: [
+							{
+								group: ['.*'],
+								message: "Don't use relative imports",
+							},
+						],
+					},
+				],
+				'prettier/prettier': 'error',
+				'unicorn/prevent-abbreviations': 'off',
+			},
+		},
+		{
+			extends: [
+				'canonical',
+				'canonical/module',
+				'canonical/browser',
+				'canonical/jsx-a11y',
+				'canonical/typescript',
+				'plugin:solid/typescript',
+				'canonical/prettier',
+			],
+			files: '*.tsx',
+			parserOptions: {
+				project: './tsconfig.json',
+			},
+			rules: {
+				'@typescript-eslint/no-throw-literal': 'off',
+				'@typescript-eslint/no-non-null-assertion': 'off',
+				'@typescript-eslint/unbound-method': 'off',
+				'@typescript-eslint/no-misused-promises': 'off',
+				'jsx-a11y/label-has-associated-control': 'off',
+				'canonical/filename-match-exported': 'off',
+				'canonical/filename-match-regex': 'off',
+				'canonical/id-match': 'off',
+				'import/no-unassigned-import': 'off',
+				'no-restricted-imports': [
+					'error',
+					{
+						patterns: [
+							{
+								group: ['.*'],
+								message: "Don't use relative imports",
+							},
+						],
+					},
+				],
+				'prettier/prettier': 'error',
+				'unicorn/prevent-abbreviations': 'off',
+			},
+		},
+	],
+	plugins: ['solid', 'import', 'prettier'],
 };
