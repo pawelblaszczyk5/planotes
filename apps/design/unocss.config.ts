ce0af2c3391d7698c4a8315b291437a637e8bb00
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-10-23 18:22:34 +0200

    Move shared unocss config into seperate package

--- apps/design/unocss.config.ts
+++ apps/design/unocss.config.ts
@@ -1,18 +1,4 @@
-import { defineConfig, presetIcons, presetWind } from 'unocss';
+import config from '@planotes/unocss';
+import { defineConfig } from 'unocss';
 
-export default defineConfig({
-	include: ['src/**/*.tsx', /.+\/ui\/.+\.jsx$/u],
-	presets: [
-		presetWind(),
-		presetIcons({
-			extraProperties: {
-				display: 'inline-block',
-				'vertical-align': 'middle',
-			},
-		}),
-	],
-	shortcuts: {
-		'bg-primary': 'bg-neutral-200 dark:bg-neutral-800',
-		'text-primary': 'text-neutral-800 dark:text-neutral-200',
-	},
-});
+export default defineConfig(config);
---
+++ packages/unocss/index.ts
@@ -0,0 +1,18 @@
+import { presetWind, presetIcons } from 'unocss';
+
+export default {
+	include: ['src/**/*.tsx', /.+\/ui\/.+\.jsx$/u],
+	presets: [
+		presetWind(),
+		presetIcons({
+			extraProperties: {
+				display: 'inline-block',
+				'vertical-align': 'middle',
+			},
+		}),
+	],
+	shortcuts: {
+		'bg-primary': 'bg-neutral-200 dark:bg-neutral-800',
+		'text-primary': 'text-neutral-800 dark:text-neutral-200',
+	},
+};
