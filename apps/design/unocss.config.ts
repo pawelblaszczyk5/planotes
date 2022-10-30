9e74c0558df36826fc5813d30ea15ae7848d4656
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-10-30 18:13:37 +0100

    More theme changes

--- apps/design/unocss.config.ts
+++ apps/design/unocss.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
 		}),
 	],
 	shortcuts: {
+		'b-primary': 'b-neutral-300 dark:b-neutral-700',
 		'bg-primary': 'bg-neutral-200 dark:bg-neutral-900',
+		'ring-primary': 'outline-none ring-sky-600 focus-visible:ring-2 dark:ring-sky-400',
 		'text-primary': 'text-neutral-900 dark:text-neutral-200',
 	},
 });
