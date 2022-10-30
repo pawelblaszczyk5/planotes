68f8331b4f9541386ae61e0cc9b1047ac950eded
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-10-30 20:20:06 +0100

    Favicon added

--- apps/web/src/root.tsx
+++ apps/web/src/root.tsx
@@ -18,13 +18,20 @@ const Root = () => {
 				<Title>Planotes</Title>
 				<Meta charset="utf-8" />
 				<Meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#171717" />
+				<meta name="msapplication-TileColor" content="#171717" />
+				<meta name="theme-color" content="#171717" />
 				<Suspense>
 					<Show when={colorScheme()}>
 						<ColorSchemeDetector colorScheme={colorScheme()!} />
 					</Show>
 				</Suspense>
 			</Head>
-			<Body class="text-primary bg-primary h-full p-4">
+			<Body class="text-primary bg-primary h-full">
 				<Suspense>
 					<ErrorBoundary>
 						<Routes>
