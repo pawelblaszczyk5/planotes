76cef840effeb76330737869c664026f71ef36a5
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-11-01 18:24:14 +0100

    Remove slowdown

--- src/routes/app/home.tsx
+++ src/routes/app/home.tsx
@@ -2,13 +2,7 @@ import { useRouteData } from 'solid-start';
 import { createServerData$ } from 'solid-start/server';
 import { requireUserId } from '~/lib/utils/session';
 
-export const routeData = () =>
-	createServerData$(async (_, { request }) => {
-		await new Promise<void>(resolve => {
-			setTimeout(() => resolve(), 13_000);
-		});
-		await requireUserId(request);
-	});
+export const routeData = () => createServerData$(async (_, { request }) => requireUserId(request));
 
 const Home = () => {
 	useRouteData<typeof routeData>()();
