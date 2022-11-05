ffb4456c60be8da0b16eef7764fa7ac37aeda4b4
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-11-05 18:21:40 +0100

    Vitest test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="vitest" />
+
 import solid from 'solid-start/vite';
 import UnoCss from 'unocss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	define: {
+		'import.meta.vitest': 'undefined',
+	},
 	plugins: [UnoCss(), solid()],
 	ssr: {
 		external: ['@prisma/client'],
+	},
+	test: {
+		includeSource: ['src/**/*.{tsx,ts}'],
 	},
 });
