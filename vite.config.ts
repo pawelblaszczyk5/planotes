f90508aa85f61927f6c819fc50b4d5ee82807da0
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-11-05 18:56:59 +0100

    Revert tests setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference types="vitest" />
-
 import solid from 'solid-start/vite';
 import UnoCss from 'unocss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	define: {
-		'import.meta.vitest': 'undefined',
-	},
 	plugins: [UnoCss(), solid()],
 	ssr: {
 		external: ['@prisma/client'],
-	},
-	test: {
-		includeSource: ['src/**/*.{tsx,ts}'],
 	},
 });
