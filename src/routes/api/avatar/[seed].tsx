404671c658940e40c22695d6988ce0b88dde8415
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-11-05 19:16:23 +0100

    Add proper license info

--- src/routes/api/avatar/[seed].tsx
+++ src/routes/api/avatar/[seed].tsx
@@ -2,7 +2,22 @@ import { Show } from 'solid-js';
 import { renderToString } from 'solid-js/web';
 import { type APIEvent } from 'solid-start';
 
-// It's all thanks to https://github.com/boringdesigners/boring-avatars
+/*
+It's all thanks to https://github.com/boringdesigners/boring-avatars
+MIT License
+
+Copyright (c) 2021 boringdesigners
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+*/
 
 const AVATAR_SIZE = 128;
 const VIEWBOX_SIZE = 36;
