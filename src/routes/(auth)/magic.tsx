b5ea246dc96c6012b06ee57643b3ea9503e43d00
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-11-17 18:32:40 +0100

    Redeeming magic token form completed

--- src/routes/(auth)/magic.tsx
+++ src/routes/(auth)/magic.tsx
@@ -1,7 +1,10 @@
-import { createSignal, onMount } from 'solid-js';
+import { createEffect, createSignal, Show } from 'solid-js';
 import { FormError, useRouteData } from 'solid-start';
 import { createServerAction$, createServerData$, redirect } from 'solid-start/server';
+import { zfd } from 'zod-form-data';
+import { Button } from '~/components/Button';
 import { db } from '~/utils/db';
+import { createFormFieldsErrors } from '~/utils/formError';
 import { createSessionCookie, getMagicIdentifier, isUserSignedIn } from '~/utils/session';
 import { isDateInPast, convertEpochSecondsToDate } from '~/utils/time';
 
@@ -16,42 +19,72 @@ export const routeData = () =>
 		return token;
 	});
 
+const redeemMagicTokenSchema = zfd.formData({
+	token: zfd.text(),
+});
+
+const FORM_ERRORS = {
+	EXPIRED_TOKEN: 'Provided token has already expired or been used, sign in again',
+	INVALID_DEVICE: "Provided token can't be matched with your device sign in again",
+	INVALID_TOKEN: 'Provided token is invalid, sign in again',
+} as const;
+
 const Magic = () => {
 	const tokenFromUrl = useRouteData<typeof routeData>();
 
 	const [formRef, setFormRef] = createSignal<HTMLFormElement>();
 
-	const [verifyMagicData, verifyMagicTrigger] = createServerAction$(async (formData: FormData, { request }) => {
-		// TODO: proper validation etc
-		const userProvidedToken = formData.get('token') as string;
+	const [redeemMagicToken, redeemMagicTokenTrigger] = createServerAction$(async (formData: FormData, { request }) => {
+		const parsedFormData = redeemMagicTokenSchema.safeParse(formData);
+
+		if (!parsedFormData.success) throw new FormError(FORM_ERRORS.INVALID_TOKEN);
+
 		const magicIdentifierId = await getMagicIdentifier(request);
 
-		if (!magicIdentifierId) throw new FormError('Error');
+		if (!magicIdentifierId) throw new FormError(FORM_ERRORS.INVALID_DEVICE);
+
+		const userProvidedToken = parsedFormData.data.token;
 
 		const magicLink = await db.magicLink.findUnique({
 			where: { id: magicIdentifierId },
 		});
 
-		if (!magicLink) throw new FormError('Error');
+		if (!magicLink) throw new FormError(FORM_ERRORS.INVALID_TOKEN);
 
 		const { validUntil, token, userId, sessionDuration } = magicLink;
 
 		if (isDateInPast(convertEpochSecondsToDate(validUntil)) || userProvidedToken !== token)
-			throw new FormError('error');
+			throw new FormError(FORM_ERRORS.EXPIRED_TOKEN);
 
 		const cookie = await createSessionCookie({ request, sessionDuration, userId });
 
 		return redirect('/app/home', { headers: { 'Set-Cookie': cookie } });
 	});
 
-	onMount(() => {
-		if (!verifyMagicData.error) formRef()?.submit();
+	const redeemMagicTokenErrors = createFormFieldsErrors(() => redeemMagicToken.error);
+
+	createEffect(() => {
+		const timeoutRef = setTimeout(() => {
+			const formData = new FormData(formRef());
+
+			if (!Object.keys(redeemMagicTokenErrors()).length) void redeemMagicTokenTrigger(formData);
+		}, 1_000);
+
+		return () => clearTimeout(timeoutRef);
 	});
 
 	return (
-		<verifyMagicTrigger.Form method="post" ref={setFormRef}>
+		<redeemMagicTokenTrigger.Form method="post" ref={setFormRef} class="contents">
+			<p class="text-secondary text-sm">
+				You'll be signed in and redirected to the application automatically. In case of problems you can also press the
+				below button yourself
+			</p>
+			<Show when={redeemMagicTokenErrors()['other']}>
+				<p class="text-destructive text-sm">{redeemMagicTokenErrors()['other']}</p>
+			</Show>
 			<input name="token" type="hidden" value={tokenFromUrl() ?? ''} />
-		</verifyMagicTrigger.Form>
+			<Button class="max-w-48 mx-auto w-full">Sign in</Button>
+		</redeemMagicTokenTrigger.Form>
 	);
 };
 
