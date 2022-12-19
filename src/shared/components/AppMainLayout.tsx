3b9785f800628643b21355d6616520582111f767
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-12-19 21:05:28 +0100

    App main layout styling adjustment

--- src/shared/components/AppMainLayout.tsx
+++ src/shared/components/AppMainLayout.tsx
@@ -1,8 +1,8 @@
 import { type JSXElement } from 'solid-js';
 
 export const AppMainLayout = (props: { children: JSXElement; heading: JSXElement }) => (
-	<div class="flex flex-col gap-6">
-		<h1 class="text-3xl">{props.heading}</h1>
+	<div class="flex flex-col">
+		<h1 class="mb-6 text-3xl">{props.heading}</h1>
 		{props.children}
 	</div>
 );
