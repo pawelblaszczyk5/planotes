fc9f861f46ba5712ab433f865f5ee87098c515a9
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-12-20 17:55:59 +0100

    Remove habits route

--- src/app/constants/routes.ts
+++ src/app/constants/routes.ts
@@ -1,6 +1,5 @@
 export const ROUTES = [
 	{ href: '/app/home', icon: 'i-lucide-home', title: 'Home' },
-	{ href: '/app/habits', icon: 'i-lucide-recycle', title: 'Habits' },
 	{ href: '/app/goals', icon: 'i-lucide-compass', title: 'Goals' },
 	{ href: '/app/tasks', icon: 'i-lucide-clipboard-check', title: 'Tasks' },
 	{ href: '/app/notes', icon: 'i-lucide-sticky-note', title: 'Notes' },
