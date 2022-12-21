082ad00933851971df28279d7f81bbb4550356a6
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-12-21 20:43:54 +0100

    Add orderBy on items fetching

--- src/routes/app/shop/(itemList).tsx
+++ src/routes/app/shop/(itemList).tsx
@@ -10,6 +10,9 @@ export const routeData = () =>
 		const userId = await requireUserId(request);
 
 		const items = await db.item.findMany({
+			orderBy: {
+				createdAt: 'desc',
+			},
 			skip: 0,
 			take: ITEMS_PER_PAGE + 1,
 			where: {
