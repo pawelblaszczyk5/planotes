f35f9a65b5ea04180d7b10e8bfc433fc82e70568
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-12-22 20:18:01 +0100

    Menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 // @refresh reload
 import * as menu from '@zag-js/menu';
 import { normalizeProps, useMachine } from '@zag-js/solid';
+import clsx from 'clsx';
 import {
 	type Accessor,
 	type JSXElement,
@@ -37,15 +38,32 @@ const DEFAULT_ROOT_PROPS = {
 
 const Root = (props: RootProps) => {
 	const propsWithDefaults = mergeProps(DEFAULT_ROOT_PROPS, props);
-	const [state, send] = useMachine(menu.machine({ id: createUniqueId() }));
+	const [state, send] = useMachine(
+		menu.machine({
+			id: createUniqueId(),
+			positioning: {
+				flip: true,
+				placement: 'bottom-end',
+			},
+		}),
+	);
 
 	const api = createMemo(() => menu.connect(state, send, normalizeProps));
 
 	return (
 		<div>
-			<button {...api().triggerProps}>{propsWithDefaults.triggerContent}</button>
-			<div {...api().positionerProps}>
-				<div {...api().contentProps}>
+			<button
+				class="ring-primary b-2 b-accent flex items-center rounded py-2 px-6 text-sm outline-offset-2"
+				{...api().triggerProps}
+			>
+				{propsWithDefaults.triggerContent}
+				<i class={clsx('ml-1 text-base', api().isOpen ? 'i-lucide-chevron-up' : 'i-lucide-chevron-down')} />
+			</button>
+			<div class="z-10" {...api().positionerProps}>
+				<div
+					class="bg-primary [&_*:not(:last-child)]:b-b-1 ring-primary max-h-[calc(41px*6)] flex-col overflow-y-auto text-sm shadow shadow-md shadow-black/50 dark:shadow-black/90"
+					{...api().contentProps}
+				>
 					<MenuContext.Provider value={api}>{propsWithDefaults.children}</MenuContext.Provider>
 				</div>
 			</div>
@@ -63,7 +81,14 @@ type ButtonItemProps = SharedItemProps;
 const ButtonItem = (props: ButtonItemProps) => {
 	const api = useApi();
 
-	return <button {...api().getItemProps({ id: props.id })}>{props.children}</button>;
+	return (
+		<button
+			class="[&[data-focus]]:text-accent b-primary block w-48 truncate py-2 px-4 text-center"
+			{...api().getItemProps({ id: props.id })}
+		>
+			{props.children}
+		</button>
+	);
 };
 
 type LinkItemProps = SharedItemProps & {
@@ -74,7 +99,11 @@ const LinkItem = (props: LinkItemProps) => {
 	const api = useApi();
 
 	return (
-		<A href={props.href} {...api().getItemProps({ id: props.id })}>
+		<A
+			class="[&[data-focus]]:text-accent b-primary block w-48 truncate py-2 px-4 text-center"
+			href={props.href}
+			{...api().getItemProps({ id: props.id })}
+		>
 			{props.children}
 		</A>
 	);
