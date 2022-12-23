57d705aab5ce6bfd73b993e3748bd399219e88e1
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-12-23 11:46:02 +0100

    Task form, wip

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,3 +1,151 @@
-export const TaskForm = () => {
-	return <p>Task form</p>;
+import { type Task, type Goal, Size, Priority } from '@prisma/client';
+import { Show, type Accessor } from 'solid-js';
+import { createServerAction$ } from 'solid-start/server';
+import { z } from 'zod';
+import { Button } from '~/components/Button';
+import { type ComboboxOption, Combobox } from '~/components/Combobox';
+import { Input } from '~/components/Input';
+import { RadioGroup } from '~/components/Radio';
+import TextEditor from '~/components/TextEditor';
+import { type FormErrors, createFormFieldsErrors } from '~/utils/form';
+import { transformHtml } from '~/utils/html';
+import { requireUserId } from '~/utils/session';
+
+type TaskFormProps = {
+	description: string;
+	goals: Array<Pick<Goal, 'id' | 'title'>>;
+	task?: Omit<Task, 'textContent'>;
+	title: string;
+};
+
+const TASK_CONTENT_MAX_LENGTH = 1_000;
+
+const FORM_ERRORS = {
+	CONTENT_REQUIRED: 'Content is required',
+	CONTENT_TOO_LONG: `Content mustn't be longer than ${TASK_CONTENT_MAX_LENGTH} characters`,
+	GOAL_ID_INVALID: 'Make sure to properly select goal from the list',
+	NAME_REQUIRED: 'Name is required',
+	NAME_TOO_SHORT: 'Name must have at least 3 characters',
+	PRIORITY_INVALID: 'Make sure to properly select priority from the list',
+	PRIORITY_REQUIRED: 'Priority is required',
+	SIZE_INVALID: 'Make sure to properly select size from the list',
+	SIZE_REQUIRED: 'Size is required',
+} satisfies FormErrors;
+
+export const TaskForm = (props: TaskFormProps) => {
+	const [upsertTask, upsertTaskTrigger] = createServerAction$(async (formData: FormData, { request }) => {
+		const userId = await requireUserId(request);
+
+		const upsertTaskSchema = z.object({
+			content: z
+				.string({ invalid_type_error: FORM_ERRORS.CONTENT_REQUIRED, required_error: FORM_ERRORS.CONTENT_REQUIRED })
+				.transform(val => transformHtml(val))
+				.superRefine(({ charactersCount }, ctx) => {
+					if (charactersCount === 0)
+						ctx.addIssue({
+							code: z.ZodIssueCode.custom,
+							message: FORM_ERRORS.CONTENT_REQUIRED,
+						});
+
+					if (charactersCount > TASK_CONTENT_MAX_LENGTH)
+						ctx.addIssue({
+							code: z.ZodIssueCode.custom,
+							message: FORM_ERRORS.CONTENT_TOO_LONG,
+						});
+				}),
+			goalId: z
+				.string({ invalid_type_error: FORM_ERRORS.GOAL_ID_INVALID })
+				.cuid(FORM_ERRORS.GOAL_ID_INVALID)
+				.optional(),
+			name: z
+				.string({ invalid_type_error: FORM_ERRORS.NAME_REQUIRED, required_error: FORM_ERRORS.NAME_REQUIRED })
+				.trim()
+				.min(3, FORM_ERRORS.NAME_TOO_SHORT),
+			priority: z.enum([Priority.LOW, Priority.MEDIUM, Priority.HIGH], {
+				invalid_type_error: FORM_ERRORS.PRIORITY_INVALID,
+				required_error: FORM_ERRORS.PRIORITY_REQUIRED,
+			}),
+			size: z.enum([Size.XS, Size.S, Size.M, Size.L, Size.XL], {
+				invalid_type_error: FORM_ERRORS.SIZE_INVALID,
+				required_error: FORM_ERRORS.SIZE_REQUIRED,
+			}),
+		});
+	});
+
+	const goalsComboboxOptions: Accessor<Array<ComboboxOption>> = () =>
+		props.goals.map(({ id, title }) => ({
+			label: title,
+			value: id,
+		}));
+
+	const selectedGoal = () =>
+		props.task ? goalsComboboxOptions().find(goalOption => goalOption.value === props.task?.goalId) ?? null : null;
+
+	const upsertTaskErrors = createFormFieldsErrors(() => upsertTask.error);
+
+	return (
+		<div class="flex max-w-3xl flex-col gap-6">
+			<h2 class="text-xl">{props.title}</h2>
+			<p class="text-secondary text-sm">{props.description}</p>
+			<upsertTaskTrigger.Form class="flex max-w-xl flex-col gap-6">
+				<Input error={upsertTaskErrors()['name']} name="name" value={props.task?.title ?? ''}>
+					Name
+				</Input>
+				<TextEditor
+					error={upsertTaskErrors()['content']}
+					name="content"
+					value={props.task?.htmlContent ?? ''}
+					maxLength={TASK_CONTENT_MAX_LENGTH}
+					class="lg:-mr-48"
+				>
+					Content
+				</TextEditor>
+				<RadioGroup.Root value={props.task?.size ?? ''} name="size">
+					<RadioGroup.Label>Size</RadioGroup.Label>
+					<div class="flex flex-wrap gap-3">
+						<RadioGroup.Item value="XS">XS</RadioGroup.Item>
+						<RadioGroup.Item value="S">S</RadioGroup.Item>
+						<RadioGroup.Item value="M">M</RadioGroup.Item>
+						<RadioGroup.Item value="L">L</RadioGroup.Item>
+						<RadioGroup.Item value="XL">XL</RadioGroup.Item>
+					</div>
+					<Show when={upsertTaskErrors()['size']}>
+						<p class="text-destructive pt-1 text-sm">{upsertTaskErrors()['size']}</p>
+					</Show>
+				</RadioGroup.Root>
+				<RadioGroup.Root value={props.task?.size ?? ''} name="priority">
+					<RadioGroup.Label>Priority</RadioGroup.Label>
+					<div class="flex flex-wrap gap-3">
+						<RadioGroup.Item value="LOW">Low</RadioGroup.Item>
+						<RadioGroup.Item value="MEDIUM">Medium</RadioGroup.Item>
+						<RadioGroup.Item value="HIGH">High</RadioGroup.Item>
+					</div>
+					<Show when={upsertTaskErrors()['priority']}>
+						<p class="text-destructive pt-1 text-sm">{upsertTaskErrors()['priority']}</p>
+					</Show>
+				</RadioGroup.Root>
+				<Show when={props.goals.length}>
+					<Combobox
+						error={upsertTaskErrors()['goalId']}
+						options={goalsComboboxOptions()}
+						maxOptions={20}
+						value={selectedGoal()}
+						name="goalId"
+					>
+						Related goal (optional)
+					</Combobox>
+				</Show>
+				<Show when={props.task}>
+					<input name="id" type="hidden" value={props.task!.id} />
+				</Show>
+				<Show when={upsertTaskErrors()['id']}>
+					<p class="text-destructive text-sm">{upsertTaskErrors()['id']}</p>
+				</Show>
+				<Show when={upsertTaskErrors()['other']}>
+					<p class="text-destructive text-sm">{upsertTaskErrors()['other']}</p>
+				</Show>
+				<Button class="max-w-48 w-full">{props.task ? 'Save note' : 'Add note'}</Button>
+			</upsertTaskTrigger.Form>
+		</div>
+	);
 };
--- src/routes/app/tasks/task/new.tsx
+++ src/routes/app/tasks/task/new.tsx
@@ -1,7 +1,39 @@
+import { useRouteData } from 'solid-start';
+import { createServerData$ } from 'solid-start/server';
 import { TaskForm } from '~/components/TaskForm';
+import { db } from '~/utils/db';
+import { requireUserId } from '~/utils/session';
+
+export const routeData = () =>
+	createServerData$(async (_, { request }) => {
+		const userId = await requireUserId(request);
+
+		const goals = await db.goal.findMany({
+			select: {
+				id: true,
+				title: true,
+			},
+			where: {
+				status: {
+					in: ['IN_PROGRESS', 'TO_DO'],
+				},
+				userId,
+			},
+		});
+
+		return goals;
+	});
 
 const NewTask = () => {
-	return <TaskForm />;
+	const data = useRouteData<typeof routeData>();
+
+	return (
+		<TaskForm
+			description="Here you can add a new task to track your progress. You can assign the task to some previously created goal (if you created one before) or just let it be standalone. Priority and size influence the prize but also positioning of tasks in list."
+			title="Add a new task"
+			goals={data() ?? []}
+		/>
+	);
 };
 
 export default NewTask;
