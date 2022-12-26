d1bc4d63ccab7f252bd121946dc53ac9305e67cb
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-12-26 21:12:07 +0100

    404 pages

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,12 +1,18 @@
 import { Title } from 'solid-start';
 import { HttpStatusCode } from 'solid-start/server';
+import { Link } from '~/components/Link';
 
 const NotFound = () => {
 	return (
-		<main>
-			<Title>Planotes | Not found :(</Title>
+		<main class="m-auto flex h-full w-full max-w-3xl flex-col items-center justify-center gap-6">
+			<Title>Are you lost? 😔 | Planotes</Title>
 			<HttpStatusCode code={404} />
-			<h1>Page Not Found</h1>
+			<h2 class="text-xl">Are you lost? 😔</h2>
+			<p class="text-secondary text-sm">
+				It looks like the link you entered is invalid. Make sure you're navigating properly and not doing some monkey
+				buisness with the URL. If you think it's not an error feel free to contact me, very appreciated!
+			</p>
+			<Link href="/">Go back to main page</Link>
 		</main>
 	);
 };
--- src/routes/app/[...404].tsx
+++ src/routes/app/[...404].tsx
@@ -1,5 +1,7 @@
-import { useRouteData } from 'solid-start';
+import { Title, useRouteData } from 'solid-start';
 import { createServerData$, HttpStatusCode } from 'solid-start/server';
+import { LinkWithIcon } from '~/components/Link';
+import { MODULE_ICONS } from '~/constants/moduleIcons';
 import { requireUserId } from '~/utils/session';
 
 export const routeData = () =>
@@ -12,8 +14,18 @@ const Lost = () => {
 
 	return (
 		<>
+			<Title>Are you lost? 😔 | Planotes</Title>
 			<HttpStatusCode code={404} />
-			<h1>Are you lost?</h1>
+			<div class="flex max-w-3xl flex-col gap-6">
+				<h2 class="text-xl">Are you lost? 😔</h2>
+				<p class="text-secondary text-sm">
+					It looks like the link you entered is invalid. Make sure you're navigating properly and not doing some monkey
+					buisness with the URL. If you think it's not an error feel free to contact me, very appreciated!
+				</p>
+				<LinkWithIcon icon={MODULE_ICONS.home} class="mr-auto" href="/app/home">
+					Go back to home
+				</LinkWithIcon>
+			</div>
 		</>
 	);
 };
